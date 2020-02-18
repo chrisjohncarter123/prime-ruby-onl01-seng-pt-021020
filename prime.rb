4d372bf7d464a8c94685de06d4edeb5a783c30f8
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-18 07:03:45 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -13,7 +13,7 @@ def prime?(number)
   counter = 1
   
   while counter < number
-    if (is_a?(number / counter))
+    if (is_a?(number / counter) Integer)
       #is divisible ...
       puts "#{number} #{counter}"
       return false
